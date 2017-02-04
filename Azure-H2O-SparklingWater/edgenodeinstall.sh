c0befb7faeb4e6676fdfe65f6dea08a2e5a58f65
mmalohlava <michal@h2o.ai>
2017-02-03 17:40:11 -0800

    Quick fixes in REST API server
    Include:
     - move scripts/conf files to structure of directories
     - nginx is listening now on 8080 port
     - rest api slightly modified to accept schema/ip/port arguments
       POST /flows/<clusterid>   - reconfigure proxy to given URL
          proto: string ("http"/"https")
          ip:    string (ip adress)
          port:  string (port)
    
        DELETE /flows/<clusterid>  - reconfigure proxy to serve default routing

--- Azure-H2O-SparklingWater/edgenodeinstall.sh
+++ Azure-H2O-SparklingWater/edgenodeinstall.sh
@@ -4,17 +4,17 @@
 echo " Configure Default Proxy Server"
 cd /etc/nginx 
 rm nginx.conf
-wget https://h2ostore.blob.core.windows.net/marketplacescripts/SparklingWater/nginx.conf
+wget https://h2ostore.blob.core.windows.net/marketplacescripts/SparklingWater/etc/nginx/nginx.conf
 cd /etc/nginx/conf.d/
-wget https://h2ostore.blob.core.windows.net/marketplacescripts/SparklingWater/h2o.conf
+wget https://h2ostore.blob.core.windows.net/marketplacescripts/SparklingWater/etc/nginx/conf.d/h2o.conf
 
 echo "Starting nginx ..."
 nginx 
 echo "Now to install Flask"
 pip install virtualenv
 pip install --upgrade Flask
-wget https://h2ostore.blob.core.windows.net/marketplacescripts/SparklingWater/EdgeNodeFlask.py
+wget https://h2ostore.blob.core.windows.net/marketplacescripts/SparklingWater/bin/EdgeNodeFlask.py
 echo "Set RESTful api"
 export FLASK_APP=EdgeNodeFlask.py
 echo " Start the FLASK RESTful API"
-python EdgeNodeFlask.py &
+nohup python EdgeNodeFlask.py >flask.log 2>&1 & 
