982636ff781c241d9a9c7680c14845244ed8e6ab
Jorge Hernandez Villapol <jherna.villapol@gmail.com>
2017-02-17 17:59:00 -0600

    SW 2.0.5 fixed, flow and edgenode url added, examples updated

--- Azure-H2O-SparklingWater/head_node_install.sh
+++ Azure-H2O-SparklingWater/head_node_install.sh
@@ -20,47 +20,27 @@ wait
 
 # Adjust based on the build of H2O you want to download. (TODO)
 
-printenv
-version=1.6
 version2=2.0
-
-SparklingBranch=rel-${version}
 SparklingBranch2=rel-${version2}
-
-echo "Fetching latest build number for branch ${SparklingBranch}..."
-curl --silent -o latest https://h2o-release.s3.amazonaws.com/sparkling-water/${SparklingBranch}/latest
-h2oBuild=`cat latest`
-
-curl --silent -o latest https://h2o-release.s3.amazonaws.com/sparkling-water/${SparklingBranch2}/latest
-h2oBuild2=`cat latest`
-
-echo "Downloading Sparkling Water version ${version}.${h2oBuild} ..."
-wget http://h2o-release.s3.amazonaws.com/sparkling-water/${SparklingBranch}/${h2oBuild}/sparkling-water-${version}.${h2oBuild}.zip &
-wait
+h2oBuild2=5
 
 wget http://h2o-release.s3.amazonaws.com/sparkling-water/${SparklingBranch2}/${h2oBuild2}/sparkling-water-${version2}.${h2oBuild2}.zip &
-wait
-
-echo "Unzipping sparkling-water-${version}.${h2oBuild}.zip ..."
-unzip -o sparkling-water-${version}.${h2oBuild}.zip 1> /dev/null &
 wait
 
 unzip -o sparkling-water-${version2}.${h2oBuild2}.zip 1> /dev/null &
 wait
 
 echo "Rename jar and Egg files"
-mv /home/h2o/sparkling-water-${version}.${h2oBuild}/py/dist/*.egg /home/h2o/sparkling-water-${version}.${h2oBuild}/py/dist/pySparkling-${version}.egg
-
 mv /home/h2o/sparkling-water-${version2}.${h2oBuild2}/assembly/build/libs/*.jar /home/h2o/sparkling-water-${version2}.${h2oBuild2}/assembly/build/libs/sparkling-water-assembly-2-0-all.jar
 mv /home/h2o/sparkling-water-${version2}.${h2oBuild2}/py/build/dist/*.egg /home/h2o/sparkling-water-${version2}.${h2oBuild2}/py/build/dist/pySparkling-${version2}.egg
 
 echo "Creating SPARKLING_HOME env ..."
 export SPARKLING_HOME="/home/h2o/sparkling-water-${version2}.${h2oBuild2}"
 export MASTER="yarn-client"
+export PYTHON_EGG_CACHE="~/"
 
 echo "Copying Sparkling folder to default storage account ... "
 hdfs dfs -mkdir -p "/H2O-Sparkling-Water-files"
-hdfs dfs -put -f /home/h2o/sparkling-water-${version}.${h2oBuild}/py/dist/*.egg /H2O-Sparkling-Water-files/
 
 hdfs dfs -put -f /home/h2o/sparkling-water-${version2}.${h2oBuild2}/assembly/build/libs/*.jar /H2O-Sparkling-Water-files/
 hdfs dfs -put -f /home/h2o/sparkling-water-${version2}.${h2oBuild2}/py/build/dist/*.egg /H2O-Sparkling-Water-files/
@@ -68,6 +48,15 @@ hdfs dfs -put -f /home/h2o/sparkling-water-${version2}.${h2oBuild2}/py/build/dis
 echo "Copying Notebook Examples to default Storage account Jupyter home folder ... "
 curl --silent -o Sentiment_analysis_with_Sparkling_Water.ipynb "https://h2ostore.blob.core.windows.net/examples/Notebooks/Sentiment_analysis_with_Sparkling_Water.ipynb"
 curl --silent -o ChicagoCrimeDemo.ipynb  "https://h2ostore.blob.core.windows.net/examples/Notebooks/ChicagoCrimeDemo.ipynb"
+
+hdfs dfs -copyToLocal /HdiApplications/ScriptActionCfgs/*.cfg cluster.cfg
+
+. cluster.cfg
+echo $EDGENODE_HTTPS_ENDPOINTS
+sed -i.backup -E  "s/@@IPADDRESS@@/$EDGENODE_HOSTS/" *.ipynb 
+sed -i.backup -E  "s,@@FLOWURL@@,$EDGENODE_HTTPS_ENDPOINTS," *.ipynb
+
+
 hdfs dfs -mkdir -p "/HdiNotebooks/H2O-PySparkling-Examples"
 hdfs dfs -put -f *.ipynb /HdiNotebooks/H2O-PySparkling-Examples/
 
